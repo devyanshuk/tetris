1264159266eed209ec144390c2cfcf30cbe59187
devyanshuk <devkoirala10@gmail.com>
2021-04-11 10:56:24 +0200

    proper line spacing

--- include/game.hpp
+++ include/game.hpp
@@ -43,25 +43,25 @@ class Tetris {
 	private:
 		/* values 0..7 in _non_moving_blocks represent static blocks of type 0..7 . -1 represents free space */
 		std::vector< std::vector<int> > _non_moving_blocks;
-		std::unique_ptr<View> _view;
-		std::optional<SDL_Keycode> _key;
+		std::unique_ptr<View>           _view;
+		std::optional<SDL_Keycode>      _key;
 
 		int         _score;
 		GameStates  _state;
 		Block       _current_active_block;
 		Uint32      _block_vertical_update_speed;
 		bool        make_new_block();
-		bool		try_moving_piece(Block test_block);
-		bool		check_collision(int new_x_pos, int new_y_pos, int new_rotation);
+		bool        try_moving_piece(Block test_block);
+		bool        check_collision(int new_x_pos, int new_y_pos, int new_rotation);
 		Uint32      _prev_y_update_time;
 		Uint32      _prev_x_update_time;
 		bool        update_screen();
-		void		remove_all_complete_rows();
+		void        remove_all_complete_rows();
 
 		/* */
 		bool        is_collision(const Block & test_block);
-		Uint32 		get_tick_difference();
+		Uint32      get_tick_difference();
 		bool        update_game();
-		void 		update_game_state();
+		void        update_game_state();
 
 };
